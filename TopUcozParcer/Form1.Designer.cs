4448f1d5075a832db8b42c659f3ba93b8af2b7e0
spbisya <spbisya@ya.ru>
2015-07-02 22:15:20 -0400

    2
    My second project

---
+++ TopUcozParcer/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace TopUcozParcer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(253, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 42);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(241, 212);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(241, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(335, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Sites Count";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(433, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(253, 68);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(288, 368);
+            this.listBox2.TabIndex = 6;
+            this.listBox2.DoubleClick += new System.EventHandler(this.listBox2_DoubleClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(253, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(378, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "    ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(334, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Title";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(6, 274);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(241, 160);
+            this.listBox3.TabIndex = 10;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            this.listBox3.DoubleClick += new System.EventHandler(this.listBox3_DoubleClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(509, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(505, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Depth";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 258);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Sites for check";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(488, 14);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(87, 258);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(167, 257);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(61, 17);
+            this.checkBox3.TabIndex = 16;
+            this.checkBox3.Text = "All sites";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 447);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Ucoz Parser by Mdgagj";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
---
+++ TopUcozParcer/Form1.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TopUcozParcer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int pages = 0;
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 51; i++) { listBox1.Items.Add("http://top.ucoz.ru/forums/" + i + "/"); }
+            listBox1.Items.RemoveAt(3);
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            textBox2.Text = listBox1.SelectedItem.ToString();
+            WebRequest req = WebRequest.Create(textBox2.Text);
+            WebResponse resp = req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+
+            doc.LoadHtml(sr.ReadToEnd());
+            try
+            {
+                var tdList = doc.DocumentNode.SelectNodes("//span");
+                String str = "";
+                foreach (var tr in tdList)
+                {
+                    //Получаем список столбцов i-ой строки
+                    var trList = tr.ChildNodes.Where(x => x.Name == "b");
+                    foreach (var td in tdList)
+                    {
+                        if (td.InnerText.Contains("Форумов")) { str = td.InnerText + "\n"; break; }
+                    }
+                    if (str.Contains("Форумов")) break;
+                }
+                String chislo = "";
+                foreach (char a in str) { if (char.IsDigit(a)) chislo += a.ToString(); }
+                int ch = int.Parse(chislo);
+                label2.Text = ch.ToString();
+                if (checkBox1.Checked) ch = int.Parse(textBox1.Text);
+                pages = ch / 30;
+                if (ch % 30 != 0) pages += 1;
+
+                var names = doc.DocumentNode.SelectNodes("//title");
+                String str1 = "";
+                foreach (var tr in names)
+                {
+                    //Получаем список столбцов i-ой строки
+
+                    if (tr.InnerText.Contains("uCoz TOP100")) { str1 = tr.InnerText + "\n"; break; }
+
+
+                }
+
+                label3.Text = str1.Replace(" - Рейтинг форумов - uCoz TOP100", "");
+            }
+            catch (Exception l)
+            {
+                textBox2.Text = l.Message;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                for (int k = 0; k < listBox3.Items.Count; k++)
+                {
+                    listBox2.Items.Add("Сайты по запросу " + listBox3.Items[k].ToString());
+                    WebRequest req = WebRequest.Create(listBox3.Items[k].ToString());
+                    WebResponse resp = req.GetResponse();
+                    Stream stream = resp.GetResponseStream();
+                    StreamReader sr = new StreamReader(stream);
+                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+
+                    doc.LoadHtml(sr.ReadToEnd());
+                    try
+                    {
+                        var tdList = doc.DocumentNode.SelectNodes("//span");
+                        String str = "";
+                        foreach (var tr in tdList)
+                        {
+                            //Получаем список столбцов i-ой строки
+                            var trList = tr.ChildNodes.Where(x => x.Name == "b");
+                            foreach (var td in tdList)
+                            {
+                                if (td.InnerText.Contains("Форумов")) { str = td.InnerText + "\n"; break; }
+                            }
+                            if (str.Contains("Форумов")) break;
+                        }
+                        String chislo = "";
+                        foreach (char a in str) { if (char.IsDigit(a)) chislo += a.ToString(); }
+                        int ch = int.Parse(chislo);
+                        if (checkBox1.Checked) ch = int.Parse(textBox1.Text);
+
+                        pages = ch / 30;
+
+                        if (ch % 30 != 0) pages += 1;
+
+
+
+                        for (int i = 1; i < pages + 1; i++)
+                        {
+                            int count = 1;
+                            String url1 = "";
+                            if (i != 1) url1 = listBox3.Items[k].ToString() + "index" + i.ToString() + ".html";
+                            else url1 = listBox3.Items[k].ToString() + "index.html";
+                            WebRequest req1 = WebRequest.Create(url1);
+                            WebResponse resp1 = req1.GetResponse();
+                            Stream stream1 = resp1.GetResponseStream();
+                            StreamReader sr1 = new StreamReader(stream1);
+                            HtmlAgilityPack.HtmlDocument doc1 = new HtmlAgilityPack.HtmlDocument();
+                            doc1.LoadHtml(sr1.ReadToEnd());
+
+                            String st1r = "";
+                            var trList1 = doc1.DocumentNode.SelectNodes("//tr");
+                            //Теперь для каждой строки tr, получаем все столбцы td
+                            foreach (var tr1 in trList1)
+                            {
+                                //Получаем список столбцов i-ой строки
+                                var tdList1 = tr1.ChildNodes.Where(x => x.Name == "td");
+                                foreach (var td1 in tdList1)
+                                {
+
+                                    if (td1.Attributes["class"].Value == "utdescr")
+                                    {
+                                        if (count > int.Parse(textBox1.Text)) { break; }
+                                        st1r = td1.InnerHtml.Replace(td1.InnerHtml.Split('\u0022')[0], "");
+                                        st1r = st1r.Replace("\"_blank\" href=\"", "");
+                                        st1r = st1r.Split('\u0022')[0];
+                                        if ((st1r != "\n") & (st1r != ""))
+                                        {
+                                            listBox2.Items.Add(st1r + "\n");
+                                            count += 1;
+                                        }
+
+                                    }
+
+                                }
+                                if (count > int.Parse(textBox1.Text)) { break; }
+                            }
+
+                        }
+                    }
+                    catch (Exception l)
+                    {
+                        listBox2.Items.Add(l.Message);
+                    }
+
+                }
+            }
+    else 
+                if (checkBox3.Checked)
+                {
+                    for (int k = 0; k < listBox1.Items.Count; k++)
+                    {
+                        
+                        
+                        WebRequest req = WebRequest.Create(listBox1.Items[k].ToString());
+                        WebResponse resp = req.GetResponse();
+                        Stream stream = resp.GetResponseStream();
+                        StreamReader sr = new StreamReader(stream);
+                        HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+
+                        doc.LoadHtml(sr.ReadToEnd());
+                        try { 
+                        var tdList = doc.DocumentNode.SelectNodes("//span");
+                        String str = "";
+                        foreach (var tr in tdList)
+                        {
+                            //Получаем список столбцов i-ой строки
+                            var trList = tr.ChildNodes.Where(x => x.Name == "b");
+                            foreach (var td in tdList)
+                            {
+                                if (td.InnerText.Contains("Форумов")) { str = td.InnerText + "\n"; break; }
+                            }
+                            if (str.Contains("Форумов")) break;
+                        }
+                        String chislo = "";
+                        foreach (char a in str) { if (char.IsDigit(a)) chislo += a.ToString(); }
+                        int ch = int.Parse(chislo);
+                        if (checkBox1.Checked) ch = int.Parse(textBox1.Text);
+
+                        pages = ch / 30;
+
+                        if (ch % 30 != 0) pages += 1;
+
+
+
+                        for (int i = 1; i < pages + 1; i++)
+                        {
+                            int count = 1;
+                            String url1 = "";
+                            if (i != 1) url1 = listBox1.Items[k].ToString() + "index" + i.ToString() + ".html";
+                            else url1 = listBox1.Items[k].ToString() + "index.html";
+                            WebRequest req1 = WebRequest.Create(url1);
+                            WebResponse resp1 = req1.GetResponse();
+                            Stream stream1 = resp1.GetResponseStream();
+                            StreamReader sr1 = new StreamReader(stream1);
+                            HtmlAgilityPack.HtmlDocument doc1 = new HtmlAgilityPack.HtmlDocument();
+                            doc1.LoadHtml(sr1.ReadToEnd());
+
+                            String st1r = "";
+                            var trList1 = doc1.DocumentNode.SelectNodes("//tr");
+                            //Теперь для каждой строки tr, получаем все столбцы td
+                            foreach (var tr1 in trList1)
+                            {
+                                //Получаем список столбцов i-ой строки
+                                var tdList1 = tr1.ChildNodes.Where(x => x.Name == "td");
+                                foreach (var td1 in tdList1)
+                                {
+
+                                    if (td1.Attributes["class"].Value == "utdescr")
+                                    {
+                                        if (count > int.Parse(textBox1.Text)) { break; }
+                                        st1r = td1.InnerHtml.Replace(td1.InnerHtml.Split('\u0022')[0], "");
+                                        st1r = st1r.Replace("\"_blank\" href=\"", "");
+                                        st1r = st1r.Split('\u0022')[0];
+                                        if ((st1r != "\n") & (st1r != ""))
+                                        {
+                                            listBox2.Items.Add(st1r + "\n");
+                                            count += 1;
+                                        }
+
+                                    }
+
+                                }
+                                if (count > int.Parse(textBox1.Text)) { break; }
+                            }
+
+                        }
+                        }
+                        catch (Exception l)
+                        {
+                            listBox2.Items.Add(l.Message);
+                        }
+
+                    }
+                }
+
+
+            else{
+            for (int i = 1; i < pages + 1; i++)
+            {
+                int count = 1;
+
+                String url = "";
+                if(i!=1)  url = textBox2.Text + "index" + i.ToString() + ".html";
+                else  url = textBox2.Text + "index.html";
+                WebRequest req = WebRequest.Create(url);
+                WebResponse resp = req.GetResponse();
+                Stream stream = resp.GetResponseStream();
+                StreamReader sr = new StreamReader(stream);
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(sr.ReadToEnd());
+                try { 
+                String str = "";
+                var trList = doc.DocumentNode.SelectNodes("//tr");
+                //Теперь для каждой строки tr, получаем все столбцы td
+                foreach (var tr in trList)
+                {
+                    //Получаем список столбцов i-ой строки
+                    var tdList = tr.ChildNodes.Where(x => x.Name == "td");
+                    foreach (var td in tdList)
+                    {
+                        
+                        if (td.Attributes["class"].Value == "utdescr")
+                        {
+                            if (count > int.Parse(textBox1.Text)) { break; }
+                            str = td.InnerHtml.Replace(td.InnerHtml.Split('\u0022')[0], "");
+                            str = str.Replace("\"_blank\" href=\"", "");
+                            str = str.Split('\u0022')[0];
+                            if ((str != "\n")&(str != ""))
+                            {
+                                listBox2.Items.Add(str + "\n");
+                                count += 1;
+                            }
+
+                        }
+                    }
+                    if (count > int.Parse(textBox1.Text)) { break; }
+                }
+                }
+                catch (Exception l)
+                {
+                    listBox2.Items.Add(l.Message);
+                }
+
+            }
+}
+         
+            
+            
+        
+        
+
+        }
+
+        private void listBox2_DoubleClick(object sender, EventArgs e)
+        {
+           // Clipboard.SetDataObject(new DataObject(listBox2.SelectedItem));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter(label3.Text.Replace(' ', '_') + ".txt"))
+            {
+                // Первой строкой записываем в файл число строк в нашем списке
+                sw.WriteLine("По запросу "+label3.Text+" нашлось "+listBox2.Items.Count.ToString()+ " сайтов");
+                for (int j = 0; j < listBox2.Items.Count; j++)
+                    sw.WriteLine(listBox2.Items[j]);
+                MessageBox.Show("Saved!", "Mdgagj");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == (char)Keys.Down)
+            {
+                for (int i = 0; i < listBox1.SelectedItems.Count; i++)
+                    listBox3.Items.Add(listBox1.SelectedItems[i]);
+            }
+        }
+
+        private void listBox3_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            listBox1.SelectionMode = SelectionMode.MultiSimple;
+            else
+                listBox1.SelectionMode = SelectionMode.One;
+        }
+    }
+}
